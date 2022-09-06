83e4f58e851851c6b6d7ec740ffd10b430c0372b
mummumgon <owl7068@gmail.com>
2022-09-06 16:49:12 +0900

    Drag and Drop 시작 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React,{useState} from "react";
 import GlovalStyle from './GlovalStyle'
-import ToDoList from "./component/TodoList";
 function App() {
   return (
     <>
-      <ToDoList/>
       <GlovalStyle/>
     </>
   );
