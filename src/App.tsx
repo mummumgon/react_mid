d11482a8f16c5bcdf659874f2c3f875db33569f1
mummumgon <owl7068@gmail.com>
2022-09-06 18:14:09 +0900

    분, 시간 교차 계산하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React,{useState} from "react";
+import { useRecoilState , useRecoilValue } from "recoil";
+import { minutes ,hourSelec} from "./atoms";
 import GlovalStyle from './GlovalStyle'
 function App() {
+  const [minuts , setMinuts] = useRecoilState(minutes);
+  const [houre , setHoure] = useRecoilState(hourSelec);
+  const onMChange =(e:React.FormEvent<HTMLInputElement>)=>{
+    const {value} = e.currentTarget;
+    setMinuts(+value);
+  };
+  const onHChange =(e:React.FormEvent<HTMLInputElement>)=>{
+    const {value} = e.currentTarget;
+    setHoure(+value);
+  };
   return (
-    <>
-      <GlovalStyle/>
-    </>
+    <div>
+      <input value={minuts} onChange={onMChange} type="number"  placeholder="minutes..."/>
+      <input value={houre} onChange={onHChange} type="number" placeholder="hours..."/>
+    </div>
   );
 }
 
--- src/atoms.ts
+++ src/atoms.ts
@@ -1,1 +1,23 @@
 import { atom, selector } from "recoil";
+
+export const minutes = atom({
+    key:'minut',
+    default:0
+})
+
+export const hourSelec = selector({
+    key:'hSelec',
+    //atom의 정해진 값을 가지고 와서 수정하여 output return하여 값을 표출 
+    get: ({get}) => {
+        const minut = get(minutes);
+        console.log(minut);
+        return minut / 60;
+    },
+    //atom자체를 수정
+    //set(1,2) 1:recoilatom 가져오기,2.새로운값
+    set:({set},newValue) =>{
+        console.log('newValue',newValue);
+        const minut = Number(newValue)*60
+        set(minutes,minut)
+    }
+});
