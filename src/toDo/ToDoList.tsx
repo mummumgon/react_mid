7e02460ae4f2b472065ced06d934dc424f953e6d
mummumgon <owl7068@gmail.com>
2022-09-14 21:50:11 +0900

    ToDo챌린지 작업 및 회원가입 폼 기능 추가

--- src/toDo/ToDoList.tsx
+++ src/toDo/ToDoList.tsx
@@ -1,9 +1,10 @@
 // 작업일 : 2022.09.12 ~ 2022.09.13
 // 작업내용: ToDoList 페이지 작업
-// Library : Recoil,react-hook-form,styled-component
-
-// 유저가 커스텀 카테고리를 생성할 수 있는 기능.
-// localStorage를 사용하여, 새로고침을 했을 때 이전의 데이터가 유지되는 기능(persistance).
+// Library : Recoil,react-hook-form,styled-component,recoil-persist
+// 기능적용: 
+// 유저가 커스텀 카테고리를 생성(기본 카테고리값 해당 selectr값이 입력됨)
+// localStorage 연결
+// ToDo 삭제기능 
 
 import { useRecoilState, useRecoilValue } from "recoil"
 import styled from "styled-components";
@@ -12,9 +13,15 @@ import ToDo from "./ToDo";
 import ToDoCreat from "./ToDoCreat";
 import ToDoHeader from "./ToDoHeader";
 
+const SelectBox = styled.div`
+  text-align: right;
+  margin: 30px 0 0;
+  border-top: 6px double ${props => props.theme.coinColor.textColor} ;
+`;
+
 function ToDoList(){
   const selecToDo = useRecoilValue(todoSelector);
-  console.log(selecToDo);
+  console.log('selecToDo',selecToDo);
   const [cate , setCate] = useRecoilState(categoryState);
   const onInput = (e:React.FormEvent<HTMLSelectElement>) =>{
     const {value} = e.currentTarget;
@@ -23,11 +30,13 @@ function ToDoList(){
   return <div><ToDoHeader/>
     <div className='container'>
     <ToDoCreat/>
-    <select value={cate} onInput={onInput}>
-      <option value={Categorys.TODO}>{Categorys.TODO}</option>
-      <option value={Categorys.DOING}>{Categorys.DOING}</option>
-      <option value={Categorys.DONE}>{Categorys.DONE}</option>
-    </select>
+    <SelectBox>
+      <select value={cate} onInput={onInput}>
+        <option value={Categorys.TODO}>{Categorys.TODO}</option>
+        <option value={Categorys.DOING}>{Categorys.DOING}</option>
+        <option value={Categorys.DONE}>{Categorys.DONE}</option>
+      </select>
+    </SelectBox>
     <ul>
       {selecToDo?.map((todo)=><ToDo {...todo}/>)}
     </ul>
