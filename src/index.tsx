5ca5033f7fbae36f6c7683a5549770e08724e864
mummumgon <owl7068@gmail.com>
2022-09-15 03:05:44 +0900

    ToDo리스트 수정 기능 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from './App';
 
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter, HashRouter} from 'react-router-dom'
 import { QueryClient,QueryClientProvider } from 'react-query'
 import {RecoilRoot} from 'recoil';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -9,9 +9,9 @@ const queryClient = new QueryClient()
 root.render(
     <RecoilRoot>
         <QueryClientProvider client={queryClient}>
-                <HashRouter>
+                <BrowserRouter>
                     <App/>
-                </HashRouter>
+                </BrowserRouter>
         </QueryClientProvider>
     </RecoilRoot>
 );
--- src/toDo/ToDo.tsx
+++ src/toDo/ToDo.tsx
@@ -1,6 +1,8 @@
 import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { Categorys, IToDo, toDoState } from "../atom";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 const LI = styled.li`
     padding: 20px;
     word-break: break-all;
@@ -23,26 +25,73 @@ const LI = styled.li`
         }
     }
 `;
+const InBtn = styled.p`
+    position: relative;
+    input{
+        padding: 2px 70px 2px 10px
+    }
+    button{
+        position: absolute;
+        right: -2px;
+        top: 50%;
+        transform: translateY(-70%);
+        height: 40px;
+        width: 50px;
+        background-color: ${props=>props.theme.coinColor.bgColor};
+        color: ${props=>props.theme.coinColor.textColor};
+        border: 0;
+    }
+`;
 function ToDo({text,id,category,category1}:IToDo){
     const REMOVE = 'REMOVE';
+    const MODIFY = 'MODIFY';
+    const SAVE = 'SAVE';
+    const [modify , setModify] = useState(false);
     const setToDo = useSetRecoilState(toDoState);
     const onClick = (e:React.MouseEvent<HTMLButtonElement>) =>{
         const {name} = e.currentTarget;
         setToDo((oldToDo)=>{
-          const idx = oldToDo.findIndex((todo)=> todo.id === id);
-          const newToDo = {id,text,category:name as Categorys,category1};
+            const idx = oldToDo.findIndex((todo)=> todo.id === id);
+            const newToDo = {id,text,category:name as Categorys,category1};
           if(name === REMOVE){
-                return [...oldToDo.slice(0,idx) ,...oldToDo.slice(idx+1)];
+              return [...oldToDo.slice(0,idx) ,...oldToDo.slice(idx+1)];
             }else{
-                return [...oldToDo.slice(0,idx), newToDo ,...oldToDo.slice(idx+1)];
+              return [...oldToDo.slice(0,idx), newToDo ,...oldToDo.slice(idx+1)];
             }
         });
-      }
+    }
+    const [inputs, setInputs] = useState({
+        changCate: '',
+        changText: ''
+    });
+    const onChange = (e:React.FormEvent<HTMLInputElement>) => {
+        const {value} = e.currentTarget;
+        const {name} = e.currentTarget;
+        setInputs({...inputs,[name]:value});
+    };
+    const onModify = () => {
+      setToDo((oldToDo)=>{
+        const idx = oldToDo.findIndex((todo)=> todo.id === id);
+        const newToDo = {id,text:inputs.changText,category,category1:inputs.changCate};
+            return [...oldToDo.slice(0,idx), newToDo ,...oldToDo.slice(idx+1)];
+        });
+      setModify((mode)=> !mode)
+      };
     return <>
         <LI>
-            {/* <p><input type="text" value={category1}/></p> */}
-          <p className="cate">{category1 === '' ? category : category1}</p>
+          {modify ?
+          <section>
+          <InBtn><input type="text" name='changCate' onChange={onChange}/><button name={SAVE} onClick={onModify}>{inputs.changCate === '' && inputs.changText === '' ? '취소' : '저장'}</button></InBtn> 
+          <input type="text" name='changText' onChange={onChange} />
+          </section> 
+          :
+          <section> 
+          <InBtn className="cate">{category1 === '' ? category : category1}<button name={MODIFY} onClick={onModify}>수정</button></InBtn>
           <p className="desc">{text}</p>
+          </section> 
+          }
+          
+          {/* <p className="cate">{category1 === '' ? category : category1}</p> */}
           <div>
           {category === Categorys.TODO || <button name={Categorys.TODO} onClick={onClick}>{Categorys.TODO}</button>}
           {category === Categorys.DOING || <button name={Categorys.DOING} onClick={onClick}>{Categorys.DOING}</button>}
